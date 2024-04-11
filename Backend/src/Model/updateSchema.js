8e3928b6dc23ccb897122135dae6807863a1ca0d
Diwanshu <diwansu531@gmail.com>
2024-04-11 16:17:07 +0530

    schema design

--- Backend/src/Model/updateSchema.js
+++ Backend/src/Model/updateSchema.js
@@ -2,5 +2,24 @@ const mongoose = require("mongoose")
 
 const updateSchema = new mongoose.Schema(
    {
-      
-   })
+      artifactLink : {
+         type:String,
+         required: true
+      },
+      videoLink : {
+         type: String,
+         required: true,
+      },
+      codiumAI : {
+         type: String,   
+      }
+
+   },
+   {versionKey: false }
+      );
+
+   const updateCapstone = (homeworkName) => {
+      return mongoose.model(homeworkName, updateSchema);
+   };
+
+   module.exports = {updateCapstone}
