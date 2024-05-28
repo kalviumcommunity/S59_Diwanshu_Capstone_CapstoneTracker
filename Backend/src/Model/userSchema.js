4530d97b1e3241a3c1b4f3472c22d0a3b56301bf
Diwanshu <diwansu531@gmail.com>
2024-05-28 11:51:34 +0545

    Schema maintained

--- Backend/src/Model/userSchema.js
+++ Backend/src/Model/userSchema.js
@@ -1,48 +1,53 @@
 const mongoose = require("mongoose");
 const argon2 = require("argon2");
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  fullname: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    require: true,
-  },
+const { Schema } = mongoose;
 
-  oauthProvider: { type: String },
-
-  oauthId: { type: String },
-
-  streak: {
-    type: Number,
-    required: true,
-    default: 0,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    oauthProvider: {
+      type: String,
+    },
+    oauthId: {
+      type: String,
+    },
+    streak: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    discussion: [{ type: Schema.Types.ObjectId, ref: "Discussion" }],
+    wiki: [{ type: Schema.Types.ObjectId, ref: "Wiki" }],
+    update: [{ type: Schema.Types.ObjectId, ref: "Homework" }],
   },
-  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  discussion: [{ type: Schema.Types.ObjectId, ref: "Discussion" }],
-  wiki: [{ type: Schema.Types.ObjectId, ref: "Wiki" }],
-  update: [{ type: Schema.Types.ObjectId, ref: "homeworkName" }],
-});
+  { timestamps: true }
+);
 
 UserSchema.index({ username: 1, email: 1 });
 
-UserSchema.methods.setpassword = async function (password) {
+UserSchema.methods.setPassword = async function (password) {
   try {
     this.password = await argon2.hash(password);
-    console.log(this);
   } catch (error) {
     throw new Error("Error hashing password");
   }
@@ -56,6 +61,6 @@ UserSchema.methods.validatePassword = async function (password) {
   }
 };
 
-const UserModel = new mongoose.model("User", UserSchema);
+const UserModel = mongoose.model("User", UserSchema);
 
 module.exports = { UserModel };
